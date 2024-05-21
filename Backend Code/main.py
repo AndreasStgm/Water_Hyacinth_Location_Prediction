4ee4aadb17ccde7eb76b3973efc44a260a1aee08
Andreas <andreassteegmans@hotmail.com>
2024-05-21 11:32:30 +0200

    training linear regression model and exporting with pickle

--- Backend Code/main.py
+++ Backend Code/main.py
@@ -2,7 +2,9 @@ import preprocessing_weather as preproc_wthr
 import preprocessing_image as preproc_img
 
 import argparse
+import pickle
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 PATH_TO_IMAGES: str = "./training_data/Sentinel2"
 PATH_TO_PROCESSED_DATA: str = "./training_data/processed_data"
@@ -67,8 +69,11 @@ def main() -> None:
         weather_df = pd.read_feather(f"{PATH_TO_PROCESSED_DATA}/preprocessed_weather_hartbeespoort.feather")
         images_df = pd.read_feather(f"{PATH_TO_PROCESSED_DATA}/preprocessed_image_test.feather")
 
-    print(weather_df)
-    print(images_df)
+    # Fitting a regression model to predict the locations
+    prediction_model = LinearRegression()
+    prediction_model.fit(weather_df, images_df)
+    # Exporting the model
+    pickle.dump(prediction_model, open("./training_data/processed_data/model.pkl", "wb"))
 
 
 if __name__ == "__main__":
